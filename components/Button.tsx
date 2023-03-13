d2b38f82d73c46fa61349d61c837174088ccad56
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2023-03-13 10:14:42 +0100

    edited

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+import styles from '@/styles/components/Button.module.css'
+
+interface ButtonProps {
+  variant: "primary" | "secondary" | "tertiary",
+  label: string,
+  onClick?: React.MouseEventHandler<HTMLButtonElement>,
+}
+
+export default function Button({ variant, label, onClick }: ButtonProps) {
+  return (
+    <button className={`${styles.buttonDefault} ${styles[variant]}`} onClick={onClick}>
+      {label}
+    </button>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
-import styles from '@/styles/Welcome.module.css'
-import Link from 'next/link'
+import styles from '@/styles/pages/Welcome.module.css'
+import { useRouter } from 'next/router';
+import Button from '@/components/Button'
 
 export default function Welcome() {
+
+  const router = useRouter();
+
+  function startProcess() {
+    router.push("/locations");
+  }
+
   return (
-    <div>
+    <div className={styles.welcome}>
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus reiciendis illo nam, labore perspiciatis, totam itaque odit temporibus atque consequatur hic at perferendis expedita nemo cumque facere voluptates dignissimos consequuntur!</p>
-      <Link href="/locations">
-        <button>Start</button>
-      </Link>
+      <Button variant="primary" label="Start" onClick={startProcess}  />
     </div>
   )
 }
