7214a535037378b55a5bae6ea24339b3897bd927
rekameszaros <rikkemeszaros@gmail.com>
2023-03-15 21:06:53 +0100

    not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
-import styles from '@/styles/pages/Welcome.module.css'
+import styles from '@/styles/pages/Welcome.module.css';
 import { useRouter } from 'next/router';
-import Button from '@/components/Button'
+import Button from '@/components/Button';
 
 export default function Welcome() {
-
   const router = useRouter();
 
   function startProcess() {
-    router.push("/locations");
+    router.push('/locations');
   }
 
   return (
     <div className={styles.welcome}>
-      <h1>Welcome</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus reiciendis illo nam, labore perspiciatis, totam itaque odit temporibus atque consequatur hic at perferendis expedita nemo cumque facere voluptates dignissimos consequuntur!</p>
+      <h1>
+        <span className="animation">Welcome</span>
+      </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus reiciendis illo nam, labore perspiciatis, totam itaque odit temporibus atque consequatur hic at perferendis expedita nemo cumque facere voluptates dignissimos consequuntur!
+      </p>
       <Button variant="primary" label="Start" onClick={startProcess} />
     </div>
-  )
+  );
 }
