3a2771f6e3d410da063a26eddd15dfec35298477
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2023-03-16 09:23:17 +0100

    edited

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -3,7 +3,27 @@ import Link from 'next/link'
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-export default function Locations() {
+export interface Locations {
+  id: number;
+  name: string;
+  status: string;
+}
+
+export async function getStaticProps() {
+
+  const res = await fetch('https://b46f027d-3a5f-4de6-9075-5e861759e531.mock.pstmn.io/locations');
+  const data = await res.json();
+
+  return {
+    props: { locations: data }
+  }
+}
+
+interface LocationsProps {
+  locations: Locations[];
+}
+
+export default function Locations({ locations }: LocationsProps) {
 
   const router = useRouter();
 
@@ -16,6 +36,13 @@ export default function Locations() {
   return (
     <div>
       <h1>Locations</h1>
+      {locations.map((location)=>{
+        return(
+          <div key={location.id}>
+            {location.name}
+          </div>
+        )
+      })}
       <Link href="/locations/1">
         <div>Dynamovej 10</div>
         <div>Status: available</div>
