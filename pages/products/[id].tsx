bae16c654d41c7eb4fdcb35c2d07a3d861214cc6
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2023-03-16 11:44:48 +0100

    edited

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -4,7 +4,33 @@ import { useEffect } from 'react';
 import Link from 'next/link'
 import Button from '@/components/Button';
 
-export default function Products() {
+export interface Products {
+  productid: number;
+  name: string;
+  description: string;
+  price: string;
+  program: number;
+}
+
+interface Context {
+  params: { id: number}
+}
+
+export async function getServerSideProps(context: Context) {
+  const id = context.params.id;
+  const res = await fetch('https://b46f027d-3a5f-4de6-9075-5e861759e531.mock.pstmn.io/products/' + id);
+  const data = await res.json();
+
+  return {
+    props: { products: data.response.products }
+  }
+}
+
+interface ProductsProps {
+  products: Products[];
+}
+
+export default function Products({ products }: ProductsProps) {
 
   const router = useRouter();
 
@@ -17,27 +43,19 @@ export default function Products() {
   function goBack() {
     router.back()
   }
-  
+
   return (
     <div className={styles.products}>
       <h1>Products</h1>
       <div>
-        <Link href="/products/1">
-          <div>Basis</div>
-          <p>Price: 59,-</p>
-        </Link>
-        <Link href="/products/1">
-          <div>Basis</div>
-          <p>Price: 59,-</p>
-        </Link>
-        <Link href="/products/1">
-          <div>Basis</div>
-          <p>Price: 59,-</p>
-        </Link>
-        <Link href="/products/1">
-          <div>Basis</div>
-          <p>Price: 59,-</p>
-        </Link>
+        {products.map((product)=>{
+          return (
+            <Link key={product.productid} href={"/start/" + product.program}>
+              <div>{product.name}</div>
+              <p>{`Price: ${product.price},-`}</p>
+            </Link>
+          );
+        })}
       </div>
       <Button variant="tertiary" label="Back" onClick={goBack} />
     </div>
